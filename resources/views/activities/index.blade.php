c7b278abcc18b972df5896dbf6245f08c433501a
Ferdian Maulana <ferdi.maulana@gmail.com>
2020-10-31 08:17:12 +0700

    benerin href

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -13,9 +13,9 @@
         <p>2. Pilih Alat yang akan Anda pinjam kemudian isi form yang disediakan.</p>
         <p>3. Booking Penggunaan Alat yang baru diterima akan masuk pada menu <b>Booking Request</b></p>
         <!-- <p>3. Booking Penggunaan Alat yang belum kami terima akan masuk pada menu <strong>Pending Request</strong>.</p> -->
-        <p>4. Kami akan melihat ketersediaan jadwal dan menentukan jadwal penggunaan untuk Anda. Anda dapat melihat jadwal yang telah disetujui pada menu <a href=""><b>Approved Schedule</b></a>. Anda harus melakukan konfirmasi terhadap jadwal telah kami setujui pada laman tersebut.</p>
-        <p>5. Apabila jadwal penuh, Anda dapat melakukan booking ulang. Silakan lihat menu <a href=""><b>Reschedule Offered List</b></a>.</p>
-        <p>6. Untuk melihat status penggunaan alat secara keseluruhan Anda dapat melihat pada menu <a href=""><strong>Status Penggunaan Alat</strong></a>.</p>
+        <p>4. Kami akan melihat ketersediaan jadwal dan menentukan jadwal penggunaan untuk Anda. Anda dapat melihat jadwal yang telah disetujui pada menu <a href="{{ route('status.approved') }}"><b>All Approved Schedule</b></a>. Anda harus melakukan konfirmasi terhadap jadwal telah kami setujui pada laman tersebut.</p>
+        <p>5. Apabila jadwal penuh, Anda dapat melakukan booking ulang. Silakan lihat menu <a href="{{ route('status.reschedule') }}"><b>Reschedule Offered List</b></a>.</p>
+        <!-- <p>6. Untuk melihat status penggunaan alat secara keseluruhan Anda dapat melihat pada menu <a href=""><strong>Status Penggunaan Alat</strong></a>.</p> -->
       </div>
     </div>
   </div>
