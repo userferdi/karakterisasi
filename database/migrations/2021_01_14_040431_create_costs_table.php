9bf3703a5b6916f5440aeedc8b81973811f08251
Ferdian Maulana <ferdi.maulana@gmail.com>
2021-01-14 16:58:45 +0700

    materials

--- database/migrations/2021_01_14_040431_create_costs_table.php
+++ database/migrations/2021_01_14_040431_create_costs_table.php
@@ -11,7 +11,7 @@ class CreateCostsTable extends Migration
         Schema::table('payments', function (Blueprint $table) {
             $table->dropColumn('quantity');
             $table->dropColumn('service');
-            // $table->dropColumn('total');
+            $table->dropColumn('total');
         });
         Schema::create('costs', function (Blueprint $table) {
             $table->bigIncrements('id');
