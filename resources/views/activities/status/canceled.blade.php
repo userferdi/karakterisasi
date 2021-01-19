740f26eff79026e487eaaebf8fbf33399a039266
Ferdian Maulana <ferdi.maulana@gmail.com>
2021-01-19 15:34:34 +0700

    liat cancel

--- resources/views/activities/status/canceled.blade.php
+++ resources/views/activities/status/canceled.blade.php
@@ -17,6 +17,7 @@
 
 @push('scripts')
 <script>
+  @role('Dosen Unpad|Dosen Non Unpad|Mahasiswa Unpad|Mahasiswa Non UnpadUser Umum')
   var detail = $('#table').DataTable({
     responsive: true,
     serverSide: true,
@@ -34,6 +35,28 @@
       {title: 'Detail', data: 'detail', name: 'detail', orderable:false, className: 'dt-center'}
     ],
   });
+  @endrole
+
+  @role('Admin')
+  var detail = $('#table').DataTable({
+    responsive: true,
+    serverSide: true,
+    scrollX: true,
+    ajax: "{{ route('status.canceled.dt') }}",
+    order: [[ 1, "asc" ]],
+    columns: [
+      {title: 'No', data: 'DT_RowIndex', name: 'no', orderable:false, className: 'dt-center'},
+      {title: 'Pengguna', data: 'user', name: 'user', className: 'dt-head-center'},
+      {title: 'No Formulir', data: 'no_form', name: 'no_form', className: 'dt-head-center'},
+      {title: 'Nama Alat', data: 'tool', name: 'tool', className: 'dt-center'},
+      {title: 'Pilihan 1', data: 'date1', name: 'date1', className: 'dt-head-center'},
+      {title: 'Pilihan 2', data: 'date2', name: 'date2', className: 'dt-head-center'},
+      {title: 'Pilihan 3', data: 'date3', name: 'date3', className: 'dt-head-center'},
+      {title: 'Status', data: 'status', name: 'status', orderable:false, className: 'dt-center'},
+      {title: 'Detail', data: 'detail', name: 'detail', orderable:false, className: 'dt-center'}
+    ],
+  });
+  @endrole;
 
   function format (d) {
     return '<table>'+
